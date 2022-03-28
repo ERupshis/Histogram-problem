71d223c1115c1759fabdc04b7ae1106c6198f27a
Эдгар Рупшис <e.rupshis@gmail.com>
2022-03-28 22:03:55 +0200

    fix in condition comparing

--- HistogramTask.cpp
+++ HistogramTask.cpp
@@ -40,7 +40,7 @@ int main()
 			int area = (j - i + 1) * sub_height;
 			if (max < area)
 				max = area;
-			
+
 		}
 	}
 
@@ -62,33 +62,25 @@ int main()
 	std::stack<std::pair<int, int>> left_border; //define left borders
 	std::vector<int> left_borders(histogram.size(), -1); // -1 - index of border right before first element in histogram
 
-	if (!histogram.empty())
-		left_border.push({ 0, histogram[0] });
-
 	for (int i = 1; i < histogram.size(); ++i)
 	{
-		while (!left_border.empty() && histogram[i] < left_border.top().second)
-		{
+		while (!left_border.empty() && histogram[i] <= left_border.top().second)
 			left_border.pop();
-		}
 
 		if (left_border.empty())
 			left_borders[i] = -1;
 		else
 			left_borders[i] = left_border.top().first;
-		
+
 		left_border.push({ i, histogram[i] });
 	}
 
 	std::stack<std::pair<int, int>> right_border; //define right borders
 	std::vector<int> right_borders(histogram.size(), histogram.size()); // 9 - index of border right after last element in histogram
 
-	if (!histogram.empty())
-		left_border.push({ 0, histogram[0] });
-
 	for (int i = histogram.size(); i > 0; --i)
 	{
-		while (!right_border.empty() && histogram[i - 1] < right_border.top().second)
+		while (!right_border.empty() && histogram[i - 1] <= right_border.top().second)
 		{
 			right_border.pop();
 		}
@@ -110,7 +102,7 @@ int main()
 		if (area > max_area)
 			max_area = area;
 
-		res.push_back(area); 
+		res.push_back(area);
 	}
 
 	std::cout << max_area << '\n';
